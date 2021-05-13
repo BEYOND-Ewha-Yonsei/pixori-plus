a66d0c72c4eea3173b59c491399b1915a9b2eb84
yewonbahn <banyaewon98@gmail.com>
2021-05-13 21:38:48 +0900

    Updates

---
+++ precache-manifest.89568315e6a182d0a47528646bff5565.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3fed298dccd8dc15d54b2f064d422ac",
+    "url": "/pixori-plus.git/index.html"
+  },
+  {
+    "revision": "1e3317b6ba1ee4c9fe09",
+    "url": "/pixori-plus.git/static/css/main.a4cfeaa8.chunk.css"
+  },
+  {
+    "revision": "f7c59b2f9b24a5a8f303",
+    "url": "/pixori-plus.git/static/js/2.161a8b10.chunk.js"
+  },
+  {
+    "revision": "31aa50b890a8b9e164eaebeeafaae8bd",
+    "url": "/pixori-plus.git/static/js/2.161a8b10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e3317b6ba1ee4c9fe09",
+    "url": "/pixori-plus.git/static/js/main.8ce2944b.chunk.js"
+  },
+  {
+    "revision": "60733d721862ae94bc17",
+    "url": "/pixori-plus.git/static/js/runtime-main.8a5a658b.js"
+  }
+]);
